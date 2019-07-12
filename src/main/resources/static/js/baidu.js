89fbb04a32297698f86ac8fd656bd2cdecf31bed
fishstormX <stormNetwork@163.com>
2019-07-12 11:53:04 +0800

    替换百度统计代码

--- src/main/resources/static/js/baidu.js
+++ src/main/resources/static/js/baidu.js
@@ -1,7 +1,9 @@
+
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?5abcac39015aa5bad12b523df0aaf8c3";
+    hm.src = "https://hm.baidu.com/hm.js?81eb5d7a9b0c92307f0f00c24c366df8";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+
