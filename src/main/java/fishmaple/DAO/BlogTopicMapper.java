c629d3b3445d49e80e653be37d01fb16161e1b3e
fish <stormnetwork>
2019-12-03 15:25:30 +0800

    添加topic的缓存层
    Signed-off-by: fish <stromnetwork@163.com>

--- src/main/java/fishmaple/DAO/BlogTopicMapper.java
+++ src/main/java/fishmaple/DAO/BlogTopicMapper.java
@@ -2,13 +2,15 @@ package fishmaple.DAO;
 
 import fishmaple.DTO.Anonymous;
 import fishmaple.DTO.BlogTopic;
+import fishmaple.conf.RedisCache4BlogConf;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
-
+@CacheNamespace(size=5,implementation = RedisCache4BlogConf.class)
 public interface BlogTopicMapper {
     public List<BlogTopic> getTopicsByUserId(@Param("userId")String userId);
     public List<BlogTopic> getAllTopics();
