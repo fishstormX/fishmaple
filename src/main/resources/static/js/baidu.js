9b0aed4721e97dda1fe5a656d37fdf49b9d52bc6
fishstormX <stormNetwork@163.com>
2020-03-10 21:48:55 +0800

    百度push网站修改

--- src/main/resources/static/js/baidu.js
+++ src/main/resources/static/js/baidu.js
@@ -16,10 +16,18 @@
     gtag('js', new Date())
     gtag('config', 'UA-143868079-1')
 
-    var bp = document.createElement('script');
-    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
+
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+
 
     var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
     document.write('<script src="' + src + '" id="sozz"><\/script>');
